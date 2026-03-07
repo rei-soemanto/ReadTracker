98726cc5e59cbfd801673e05cdf52117fbd75f43
Rei Soemanto <reresoemanto@gmail.com>
2026-03-07 10:11:18 +0700

    week 4 material

--- ReadTracker/Views/AddReadView.swift
+++ ReadTracker/Views/AddReadView.swift
@@ -14,44 +14,44 @@ struct AddReadView: View {
     @State var isReadyToRead = false
     
     var body: some View {
-        VStack(spacing: 10) {
-            TextField("Search for a book", text: $searchText, onEditingChanged: { isEditing in
-                showSuggestions = !searchText.isEmpty
-            })
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding([.leading, .bottom, .trailing])
-            
-            HStack {
-                Text("Book info").font(.title).fontWeight(.bold)
-                Spacer()
-            }.padding(.horizontal)
-            
-            if searchText.isEmpty || isResultEmpty {
-                ZStack {
-                    Rectangle()
-                        .fill(Color(red: 0.973, green: 0.973, blue: 0.973))
-                        .frame(width: 360, height: 222)
-                    
-                    Text("No book information").font(.body)
-                }
-            } else if showSuggestions {
-                ScrollView {
-                    VStack {
-                        Button(action: {
-                            isReadyToRead = true
-                            showSuggestions = false
-                        }) {
-                            BookCardView()
+        if isReadyToRead {
+            StartReadView()
+        } else {
+            VStack(spacing: 10) {
+                TextField("Search for a book", text: $searchText, onEditingChanged: { isEditing in
+                    showSuggestions = searchText.isEmpty
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding([.leading, .bottom, .trailing])
+                
+                HStack {
+                    Text("Book info").font(.title).fontWeight(.bold)
+                    Spacer()
+                }.padding(.horizontal)
+                
+                if searchText.isEmpty || isResultEmpty {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color(red: 0.973, green: 0.973, blue: 0.973))
+                            .frame(width: 360, height: 222)
+                        
+                        Text("No book information").font(.body)
+                    }
+                } else if showSuggestions {
+                    ScrollView {
+                        VStack {
+                            Button(action: {
+                                isReadyToRead = true
+                                showSuggestions = false
+                            }) {
+                                BookCardView()
+                            }
                         }
                     }
                 }
+                
+                Spacer()
             }
-            
-            if isReadyToRead {
-                StartReadView()
-            } 
-            
-            Spacer()
         }
     }
 }
